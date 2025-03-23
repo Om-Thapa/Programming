649be6306da5df4d96b6ef5cb8569a6a8b5a2ccc
Om Thapa <omthapa1904@gmail.com>
2025-03-23 15:58:22 +0530

    Remove unused C++ files and add new problem solutions for 2D arrays and strings

---
+++ DSA/C++/Problems/2D_Arrays/P240.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>> mat, int target){
+    int m = mat.size(), n = mat[0].size();
+
+    int r = 0, c = n-1;
+    while(r < m && c >= 0){
+        if(mat[r][c] == target) return true;
+        else if(mat[r][c] > target) c--;
+        else r++;
+    }
+}
---
+++ DSA/C++/Problems/Strings/P443.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int compress(vector<char> &chars){//O(n)
+    int n = chars.size();
+    int idx = 0;
+
+    for(int i=0; i<n; i++){
+        char ch = chars[i];
+        int count = 0;
+
+        while(i<n && chars[i++] == ch){
+            count++;
+        }
+
+        if(count == 1){
+            chars[idx++] = ch;
+        }else{
+            chars[idx++] = ch;
+            string str = to_string(count);
+            for(char dig : str){
+                chars[idx++] = dig;
+            }
+        }
+
+        i--;
+    }
+
+    chars.resize(idx);
+    return idx;
+}
